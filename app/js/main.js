12174febba2101b4cd5a4c7cdf60ba4fed0714a0
Honor <mytwink21@mail.ru>
2022-04-12 18:43:18 +0100

    Секция header

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,7 @@
-import {testFunction} from './scripts/test';
-testFunction();
+const burger = document.querySelector('.js-burger');
+const openMenu = document.querySelector('.js-nav');
+
+burger.addEventListener('click', function(){
+	burger.classList.toggle('_active');
+	openMenu.classList.toggle('_active');
+})
